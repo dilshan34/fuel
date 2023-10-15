350a62753c766ab3dd38c66aa9a0bc75e9d01eba
dilshan34 <dilshansachinthana34@gmail.com>
2023-10-15 19:26:10 +0530

    config database and run first request

--- backend/Order Service/src/main/java/fuel/orderservice/Controller/FuelRegisterController.java
+++ backend/Order Service/src/main/java/fuel/orderservice/Controller/FuelRegisterController.java
@@ -1,8 +1,18 @@
 package fuel.orderservice.Controller;
 
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/register")
 public class FuelRegisterController {
-    //System.out.println("hi");
+
+
+    @PostMapping("/echo")
+    public String registerFuelStation(){
+
+        return "Registered Successfully";
+    }
 }
