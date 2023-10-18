28e8542db701c7e39c0400bee8d396b902866d0e
dilshan34 <dilshansachinthana34@gmail.com>
2023-10-18 18:38:16 +0530

    get all fuel stations API

--- backend/Order Service/src/main/java/fuel/orderservice/Controller/FuelRegisterController.java
+++ backend/Order Service/src/main/java/fuel/orderservice/Controller/FuelRegisterController.java
@@ -6,6 +6,8 @@ import fuel.orderservice.Service.FuelRegisterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class FuelRegisterController {
 
@@ -17,8 +19,14 @@ public class FuelRegisterController {
         return "Registered Successfully";
     }
 
-    @RequestMapping(value = "/register",method = RequestMethod.POST,consumes = {"application/json"},produces = {"application/json"})
-    public FuelRegister createFuelStation(@RequestBody FuelRegister fuelRegister){
+    //Register a fuel station
+    @RequestMapping(value = "/register", method = RequestMethod.POST, consumes = {"application/json"}, produces = {"application/json"})
+    public FuelRegister createFuelStation(@RequestBody FuelRegister fuelRegister) {
         return fuelRegisterService.addFuelStation(fuelRegister);
+    }
+
+    @RequestMapping(value = "/getStations", method = RequestMethod.GET, consumes = {"application/json"}, produces = {"application/json"})
+    public List<FuelRegister> getFuelStations() {
+        return fuelRegisterService.getFuelStations();
     }
 }
--- backend/Order Service/src/main/java/fuel/orderservice/Service/FuelRegisterService.java
+++ backend/Order Service/src/main/java/fuel/orderservice/Service/FuelRegisterService.java
@@ -5,6 +5,8 @@ import fuel.orderservice.Repository.FuelRegisterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class FuelRegisterService {
 
@@ -13,5 +15,9 @@ public class FuelRegisterService {
 
     public FuelRegister addFuelStation(FuelRegister fuelRegister){
         return fuelRegisterRepository.save(fuelRegister);
+    }
+
+    public List<FuelRegister> getFuelStations( ){
+        return fuelRegisterRepository.findAll();
     }
 }
